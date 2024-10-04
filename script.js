f6724c87da20f1d66f4601757d5ba18f674e0d50
akint980 <akintunde_aderonke@yahoo.com>
2024-10-03 22:49:27 -0500

    update

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 37.7749, lng: -122.4194 }, // Center on San Francisco
-        zoom: 12,
-    });
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
 }
 
-window.onload = initMap; // Ensure this is called after the page loads
+initMap();
